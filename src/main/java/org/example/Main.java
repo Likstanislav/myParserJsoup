b9765e7679d436b7ed090f41828bd097b23fa759
likstanislav <likstanislav@yandex.ru>
2023-03-13 10:00:49 +0700

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String html = "https://google.com";
+        Document document = Jsoup.connect(html).get();
+        Elements elements = document.select("a");
+        for (Element el : elements) {
+            System.out.println(el.attr("href"));
+        }
     }
 }
